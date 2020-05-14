ca007da1735e52d3d007ded2f0c6b2f83acf5fe3
Francesco <francescodtroiano@gmail.com>
2020-05-14 17:09:43 -0500

    passing test and keith said so

--- LINQ.Tests/LINQ.Tests.cs
+++ LINQ.Tests/LINQ.Tests.cs
@@ -1,6 +1,10 @@
 using System;
+using System.Diagnostics.Tracing;
 using System.IO;
+using System.IO.Enumeration;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Threading;
 using Newtonsoft.Json;
 using Xunit;
 
@@ -8,15 +12,19 @@ namespace LINQ.Tests
 {
     public class UnitTest1
     {
+
+        static string filename = "data.json";
+        static string data = File.ReadAllText(filename);
+
+        RootObject geojson = JsonConvert.DeserializeObject<RootObject>(data);
+
         [Fact]
         public void CapableOfReading()
         {
-            string filename = "data.json";
-            string data = File.ReadAllText(filename);
+
 
             Assert.NotEmpty(data);
 
-            RootObject geojson = JsonConvert.DeserializeObject<RootObject>(data);
 
             Assert.NotNull(geojson);
 
@@ -28,6 +36,35 @@ namespace LINQ.Tests
                 );
 
             Assert.Equal(147, geojson.features.Count());
+
+        }
+
+        [Fact]
+        public void DataFirstAndLastInListIsCorrect()
+        {
+
+            var query = from feature 
+                        in geojson.features 
+                        select feature.properties.neighborhood;
+
+            int counter = 0;
+
+            foreach (string neighborhood in query)
+            {
+                counter++;
+            }
+
+
+
+            Assert.Equal("10001", geojson.features.First().properties.zip);
+            Assert.Equal("10292", geojson.features.Last().properties.zip);
+            Assert.Equal(147, counter);
+
+        }
+
+        [Fact]
+        public void FilterNeighborhoodsWithNoNames()
+        {
 
         }
     }
